d99abd2d3b6fe0bf5cabbfd4a9d2d4fd38dbfbdc
TakWolf <takwolf@foxmail.com>
2023-05-10 20:09:58 +0800

    完成 ksx1001 实现和测试用例

---
+++ src/character_encoding_utils/ksx1001.py
@@ -0,0 +1,102 @@
+
+_euc_offset = 0xA0
+
+
+class KSX1001Exception(Exception):
+    pass
+
+
+def query_chr(row: int, col: int):
+    if row < 1 or row > 94 or col < 1 or col > 94:
+        raise KSX1001Exception(f"'row' and 'col' must between 1 and 94")
+    # 'euc_kr' 编码器对字符 'Hangul Filler (0x3164)' 的处理不正确，但是官方开发者竟然认为这不是一个错误
+    # 问题详情见： https://github.com/python/cpython/issues/101863
+    if row == 4 and col == 52:
+        return chr(0x3164)
+    try:
+        return bytes([row + _euc_offset, col + _euc_offset]).decode('ksx1001')
+    except UnicodeDecodeError as e:
+        raise KSX1001Exception(f'ksx1001 char at ({row}, {col}) undefined') from e
+
+
+def query_coord(c: str) -> tuple[int, int]:
+    if len(c) != 1:
+        raise KSX1001Exception('Must be one char')
+    try:
+        encoded = c.encode('ksx1001')
+    except UnicodeEncodeError as e:
+        raise KSX1001Exception(f"'{c}' not a ksx1001 char") from e
+    if len(encoded) == 1:
+        raise KSX1001Exception(f"'{c}' is a ascii char")
+    row = encoded[0] - _euc_offset
+    col = encoded[1] - _euc_offset
+    return row, col
+
+
+def get_categories() -> list[str]:
+    return ['other', 'syllable', 'hanja']
+
+
+def query_category(c: str) -> str | None:
+    try:
+        row = query_coord(c)[0]
+    except KSX1001Exception:
+        return None
+
+    if 1 <= row <= 12:
+        return 'other'
+    elif 16 <= row <= 40:
+        return 'syllable'
+    elif 42 <= row <= 93:
+        return 'hanja'
+    else:
+        raise AssertionError(f'Impossible row: {row}')
+
+
+def _get_alphabet_by_rows_between(row_start, row_end) -> list[str]:
+    alphabet = []
+    for row in range(row_start, row_end + 1):
+        for col in range(1, 94 + 1):
+            try:
+                alphabet.append(query_chr(row, col))
+            except KSX1001Exception:
+                pass
+    return alphabet
+
+
+_alphabet_other = _get_alphabet_by_rows_between(1, 12)
+_alphabet_syllable = _get_alphabet_by_rows_between(16, 40)
+_alphabet_hanja = _get_alphabet_by_rows_between(42, 93)
+_alphabet = _alphabet_other + _alphabet_syllable + _alphabet_hanja
+
+
+def get_alphabet_other() -> list[str]:
+    return list(_alphabet_other)
+
+
+def get_alphabet_syllable() -> list[str]:
+    return list(_alphabet_syllable)
+
+
+def get_alphabet_hanja() -> list[str]:
+    return list(_alphabet_hanja)
+
+
+def get_alphabet() -> list[str]:
+    return list(_alphabet)
+
+
+def get_other_count() -> int:
+    return len(_alphabet_other)
+
+
+def get_syllable_count() -> int:
+    return len(_alphabet_syllable)
+
+
+def get_hanja_count() -> int:
+    return len(_alphabet_hanja)
+
+
+def get_count() -> int:
+    return len(_alphabet)
--- tests/test_ksx1001.py
+++ tests/test_ksx1001.py
@@ -1,3 +1,90 @@
+import pytest
 
-def test():
-    pass
+from character_encoding_utils import ksx1001
+from character_encoding_utils.ksx1001 import KSX1001Exception
+
+
+def test_query_chr():
+    assert ksx1001.query_chr(1, 50) == '⌒'
+    assert ksx1001.query_chr(16, 1) == '가'
+    assert ksx1001.query_chr(35, 40) == '쳰'
+    assert ksx1001.query_chr(50, 54) == '壟'
+    assert ksx1001.query_chr(93, 94) == '詰'
+
+    with pytest.raises(KSX1001Exception):
+        ksx1001.query_chr(-1, 50)
+    with pytest.raises(KSX1001Exception):
+        ksx1001.query_chr(20, 500)
+
+    with pytest.raises(KSX1001Exception) as info:
+        ksx1001.query_chr(94, 94)
+    assert isinstance(info.value.__cause__, UnicodeDecodeError)
+
+
+def test_query_coord():
+    assert ksx1001.query_coord('ㆌ') == (4, 92)
+    assert ksx1001.query_coord('φ') == (5, 85)
+    assert ksx1001.query_coord('떰') == (22, 22)
+    assert ksx1001.query_coord('틘') == (38, 24)
+    assert ksx1001.query_coord('紺') == (42, 90)
+    assert ksx1001.query_coord('絿') == (47, 29)
+
+    with pytest.raises(KSX1001Exception):
+        ksx1001.query_coord('abc')
+    with pytest.raises(KSX1001Exception):
+        ksx1001.query_coord('d')
+
+    with pytest.raises(KSX1001Exception) as info:
+        ksx1001.query_coord('😈')
+    assert isinstance(info.value.__cause__, UnicodeEncodeError)
+
+
+def test_query_category():
+    categories = ksx1001.get_categories()
+    assert len(categories) == 3
+    assert 'other' in categories
+    assert 'syllable' in categories
+    assert 'hanja' in categories
+
+    assert ksx1001.query_category('ぜ') == 'other'
+    assert ksx1001.query_category('룝') == 'syllable'
+    assert ksx1001.query_category('絿') == 'hanja'
+    assert ksx1001.query_category('A') is None
+    assert ksx1001.query_category('😈') is None
+
+
+def test_alphabet():
+    alphabet = ksx1001.get_alphabet_other()
+    assert len(alphabet) == 988
+    for c in alphabet:
+        row, col = ksx1001.query_coord(c)
+        assert ksx1001.query_chr(row, col) == c
+        assert ksx1001.query_category(c) == 'other'
+
+    alphabet = ksx1001.get_alphabet_syllable()
+    assert len(alphabet) == 2350
+    for c in alphabet:
+        row, col = ksx1001.query_coord(c)
+        assert ksx1001.query_chr(row, col) == c
+        assert ksx1001.query_category(c) == 'syllable'
+
+    alphabet = ksx1001.get_alphabet_hanja()
+    assert len(alphabet) == 4888
+    for c in alphabet:
+        row, col = ksx1001.query_coord(c)
+        assert ksx1001.query_chr(row, col) == c
+        assert ksx1001.query_category(c) == 'hanja'
+
+    alphabet = ksx1001.get_alphabet()
+    assert len(alphabet) == 8226
+    for c in alphabet:
+        row, col = ksx1001.query_coord(c)
+        assert ksx1001.query_chr(row, col) == c
+        assert ksx1001.query_category(c) is not None
+
+
+def test_count():
+    assert ksx1001.get_other_count() == 988
+    assert ksx1001.get_syllable_count() == 2350
+    assert ksx1001.get_hanja_count() == 4888
+    assert ksx1001.get_count() == 8226
